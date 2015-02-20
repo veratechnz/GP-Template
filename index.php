72e8b6f376e0a06b27fc4a08837f739e6fbf041e
Jared Mac <axion8@gmail.com>
2015-02-20 15:52:07 +1300

    thisisnew

--- index.php
+++ index.php
@@ -6,6 +6,36 @@
 	<title>GlassProtech</title>
 </head>
 <body>
-	
+
+
+	<div class="container-fluid">
+
+		<div class="row">
+			<div class="col-md-12 top-panel"></div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-12 logo-panel"></div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-12 nav-bar"></div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-12 slider"></div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-12 content"></div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-12 footer"></div>
+		</div>
+		
+	</div>
+
+
 </body>
 </html>
