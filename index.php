b0a9708cfdce731246a800e0b36dbfb4f7552381
Jared Mac <axion8@gmail.com>
2015-02-20 15:23:43 +1300

    first-commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+	<title>GlassProtech</title>
+</head>
+<body>
+	
+</body>
+</html>
