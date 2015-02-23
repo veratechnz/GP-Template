8247f2d389f8f3eeef4daa283c3e7f1d6c2784dc
Jared Mac <axion8@gmail.com>
2015-02-24 12:21:53 +1300

    1st commit-mergeb

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+// All Custom JS
+
+// Nivo  Slider Code
+$(window).load(function() {
+	$('#slider').nivoSlider({
+		effect: 'fade',                                   
+    	animSpeed: 2000,                 
+    	pauseTime: 5000,                
+    	directionNav: false,             
+    	controlNav: false,               
+    	controlNavThumbs: false,        
+    	pauseOnHover: true,             
+    	manualAdvance: false,
+    	prevText: '',
+        nextText: ''                
+	});
+});
+
