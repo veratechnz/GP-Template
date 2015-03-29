216e9d8740dfae15d0e8d8f5994a7af54bb0c370
cmweb <cmweb@mini.telecom>
2015-03-30 12:02:19 +1300

    backup 30/03/2015 12:00pm

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,55 @@
 // All Custom JS
 
-// Nivo  Slider Code
-$(window).load(function() {
+// Nivo  Slider Call
+if (document.getElementById('slider')) {
+  $(window).load(function() {
+
+    $('#slider').nivoSlider({
+      effect: 'slideInRight',                          
+        animSpeed: 700,                 
+        pauseTime: 4000,                
+        directionNav: false,             
+        controlNav: false,               
+        controlNavThumbs: false,        
+        pauseOnHover: false,             
+        manualAdvance: false,
+        prevText: 'PREV',
+        nextText: 'NEXT'                
+    });
+
+  }); //Window onload ends
+};
+
+$(function() {
+  $('#who-we-are-accordion').on('hidden.bs.collapse', toggleChevron);
+  $('#who-we-are-accordion').on('shown.bs.collapse', toggleChevron);
+});
+
+function toggleChevron(e) {
+      $(e.target)
+        .prev('.panel-heading')
+        // .find('i.indicator')
+        // .toggleClass('glyphicon-minus glyphicon-plus');
+        $('#accordion .panel-heading').css('background-color', 'green');
+        $('#accordion .panel-heading').removeClass('highlight');
+        $(e.target).prev('.panel-heading').addClass('highlight');
+}
 
 
-	$('#slider').nivoSlider({
-		effect: 'fade',                                   
-    	animSpeed: 2000,                 
-    	pauseTime: 5000,                
-    	directionNav: false,             
-    	controlNav: false,               
-    	controlNavThumbs: false,        
-    	pauseOnHover: true,             
-    	manualAdvance: false,
-    	prevText: '',
-        nextText: ''                
-	});
+// function setAccordionColorsExpanded() {
+//   // $('.accordion').on('hidden.bs.collapse', function () {
+    
+//   // })
+//   $('.accordion .panel-heading').click(function() {
+//     $(this).collapse('toggle');
+//     $(this).css('background-color', '#EE2A2B');
 
+//     // console.log(this);
+//     // if ( $(this + ' a[aria-expanded]') === 'true' ) {
+//     //   $(this + ' a').click();
+//     //   $(this).css('background-color', '#EE2A2B');
+//     // }
+//   });
+// }
 
-}); //Window onload ends
 
