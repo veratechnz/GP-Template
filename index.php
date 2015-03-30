300d3d78d2c13ba9cf6f1626bca6bea43ccecb3e
veratech <jared@veratech.co.nz>
2015-03-30 18:49:28 +1300

    300315c

--- index.php
+++ index.php
@@ -63,46 +63,10 @@
 				</div>
 			</div>
 		</div>
-		<!-- FOOTER -->
-		<div class="row footer">
-			<div class="container">
-				<!-- LOGO -->
-				<div class="col-sm-3">
-					<img src="img/logo.png" alt="Glass Protech Logo" >
-				</div>
-				<!-- CONTACT US -->
-				<div class="col-sm-3 contact-us">
-					<h4 class="footer-title">Contact Us</h4>
-					<p><i class="fa fa-phone fa-lg"></i>0800 457 776</p>
-					<p class="email"><i class="fa fa-envelope"></i><a href="mailto:info@glassprotech.co.nz">info@glassprotech.co.nz</a></p>
-					<p><i class="fa fa-map-marker fa-lg"></i>&nbsp;At an address.<br><span class="indent">Somewhere in</span><br><span class="indent">New Zealand.</span></p>
-				</div>
-				<!-- WHAT WE DO -->
-				<div class="col-sm-3">
-					<h4 class="footer-title">What We Do</h4>
-					<p>Sed ut per spiciat it  unde om nis iste natus error sit volup tat em acc usant ium do.</p>
-					<p class="footer-sub-title">Sign Up to Newsletter</p>
-					<div class="input-group">
-						<input type="text" class="form-control" placeholder="Email Address" aria-describedby="basic-addon2">
-						<span class="input-group-addon" id="basic-addon2">SUBMIT</span>
-					</div>
-				</div>
-				<!-- LOGIN -->
-				<div class="col-sm-3">
-					<h4 class="footer-title">Licencee Login</h4>
-					<input type="text" class="form-control" placeholder="Email Address">
-					<hr>
-					<input type="text" class="form-control" placeholder="Password">
-					<button type="button" class="btn btn-default" aria-label="login">LOGIN</button>
-				</div>
-			</div>
-		</div>
-		<!-- BOTTOM PANEL -->
-		<div class="row bottom-panel">
-			<div class="col-xs-12 text-center">
-				<p>All Content Copyright - GlassProtech 2015</p>
-			</div>
-		</div>
+
+<!-- FOOTER -->
+<?php include('inc/footer-inc.php') ?>
+
 	</div><!-- main wrapper ends -->
 
 <!-- All JS Files -->
--- licensee.php
+++ licensee.php
@@ -12,32 +12,6 @@
 
 	<!-- MAIN WRAPPER -->
 	<div class="container-fluid">
-		<!-- TOP PANEL -->
-		<div class="row top-panel">
-			<div class="container">
-				<!-- Contact Details -->
-				<div class="col-xs-8 contact-details">
-					<i class="fa fa-phone"></i>&nbsp;&nbsp;0800 457 776
-					<span class="email">
-						<i class="fa fa-envelope"></i>&nbsp;&nbsp;<a href="mailto:info@glassprotech.co.nz">info@glassprotech.co.nz</a>
-					</span>
-				</div>
-				<!-- Login -->
-				<div class="col-xs-1 extra-gutter"></div>
-				<!-- <div class="col-xs-1 extra-gutter"></div> -->
-				<div class="col-xs-3 login">Licencee Login&nbsp;&nbsp;<i class="fa fa-shopping-cart"></i></div>
-			</div>
-		</div>
-		<!-- LOGO PANEL -->
-		<div class="row logo-panel">
-			<div class="container">
-				<div class="col-md-6">
-					<div class="logo">
-						<img src="img/logo.png" alt="Glass Protech Logo" >
-					</div>
-				</div>
-			</div>
-		</div>
 
 <!-- TOP PANEL -->
 <?php include('inc/logo-inc.php') ?>
@@ -180,46 +154,10 @@
 				</div>
 			</section>
 		</div>
-		<!-- FOOTER -->
-		<div class="row footer">
-			<div class="container">
-				<!-- LOGO -->
-				<div class="col-sm-3">
-					<img src="img/logo.png" alt="Glass Protech Logo" >
-				</div>
-				<!-- CONTACT US -->
-				<div class="col-sm-3 contact-us">
-					<h4 class="footer-title">Contact Us</h4>
-					<p><i class="fa fa-phone fa-lg"></i>0800 457 776</p>
-					<p class="email"><i class="fa fa-envelope"></i><a href="mailto:info@glassprotech.co.nz">info@glassprotech.co.nz</a></p>
-					<p><i class="fa fa-map-marker fa-lg"></i>&nbsp;At an address.<br><span class="indent">Somewhere in</span><br><span class="indent">New Zealand.</span></p>
-				</div>
-				<!-- WHAT WE DO -->
-				<div class="col-sm-3">
-					<h4 class="footer-title">What We Do</h4>
-					<p>Sed ut per spiciat it  unde om nis iste natus error sit volup tat em acc usant ium do.</p>
-					<p class="footer-sub-title">Sign Up to Newsletter</p>
-					<div class="input-group">
-						<input type="text" class="form-control" placeholder="Email Address" aria-describedby="basic-addon2">
-						<span class="input-group-addon" id="basic-addon2">SUBMIT</span>
-					</div>
-				</div>
-				<!-- LOGIN -->
-				<div class="col-sm-3">
-					<h4 class="footer-title">Licencee Login</h4>
-					<input type="text" class="form-control" placeholder="Email Address">
-					<hr>
-					<input type="text" class="form-control" placeholder="Password">
-					<button type="button" class="btn btn-default" aria-label="login">LOGIN</button>
-				</div>
-			</div>
-		</div>
-		<!-- BOTTOM PANEL -->
-		<div class="row bottom-panel">
-			<div class="col-xs-12 text-center">
-				<p>All Content Copyright - GlassProtech 2015</p>
-			</div>
-		</div>
+
+<!-- FOOTER -->
+<?php include('inc/footer-inc.php') ?>
+
 	</div><!-- main wrapper ends -->
 
 <!-- All JS Files -->
