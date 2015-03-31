e2af6920f3a77b86d87c7d3bb1ad7ed6aae53a7f
cmweb <cmweb@mini.telecom>
2015-03-31 21:08:45 +1300

    31/03/2015 21:15

--- js/custom.js
+++ js/custom.js
@@ -5,18 +5,27 @@ if (document.getElementById('slider')) {
   $(window).load(function() {
 
     $('#slider').nivoSlider({
-      effect: 'slideInRight',                          
-        animSpeed: 700,                 
-        pauseTime: 4000,                
-        directionNav: false,             
-        controlNav: false,               
-        controlNavThumbs: false,        
-        pauseOnHover: false,             
-        manualAdvance: false,
-        prevText: 'PREV',
-        nextText: 'NEXT'                
+      effect: 'fade',                 // Specify sets like: 'fold,fade,sliceDown'
+      slices: 15,                     // For slice animations
+      boxCols: 8,                     // For box animations
+      boxRows: 4,                     // For box animations
+      animSpeed: 1200,                 // Slide transition speed
+      pauseTime: 3500,                 // How long each slide will show
+      startSlide: 0,                     // Set starting Slide (0 index)
+      directionNav: true,             // Next & Prev navigation
+      controlNav: false,                 // 1,2,3... navigation
+      controlNavThumbs: false,         // Use thumbnails for Control Nav
+      pauseOnHover: true,             // Stop animation while hovering
+      manualAdvance: false,             // Force manual transitions
+      prevText: '<i class="fa fa-chevron-left fa-4x"></i>', // Prev directionNav text
+      nextText: '<i class="fa fa-chevron-right fa-4x"></i>', // Next directionNav text
+      randomStart: false,             // Start on a random slide
+      beforeChange: function(){},     // Triggers before a slide transition
+      afterChange: function(){},         // Triggers after a slide transition
+      slideshowEnd: function(){},     // Triggers after all slides have been shown
+      lastSlide: function(){},         // Triggers when last slide is shown
+      afterLoad: function(){}         // Triggers when slider has loaded      
     });
-
   }); //Window onload ends
 };
 
